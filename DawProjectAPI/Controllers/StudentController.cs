aaabc2e3acd18364b0afe642afac603e15518e9f
Dennis-Soare <79460847+Dennis-Soare@users.noreply.github.com>
2024-02-22 14:52:19 +0200

    added response types

--- DawProjectAPI/Controllers/StudentController.cs
+++ DawProjectAPI/Controllers/StudentController.cs
@@ -10,12 +10,16 @@ namespace DawProjectAPI.Controllers
     public class StudentController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<StudentDTO>> GetStudents()
         {
             return Ok(StudentStore.studentList);
         }
 
         [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<StudentDTO> GetStudent(int id)
         {
             if (id == 0)
