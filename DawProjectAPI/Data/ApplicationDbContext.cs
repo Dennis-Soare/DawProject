87558ba1d3d188cd980a05331fc0c014c606c006
Dennis-Soare <79460847+Dennis-Soare@users.noreply.github.com>
2024-02-22 22:49:33 +0200

    added database connection

--- DawProjectAPI/Data/ApplicationDbContext.cs
+++ DawProjectAPI/Data/ApplicationDbContext.cs
@@ -5,6 +5,9 @@ namespace DawProjectAPI.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
         public DbSet<Student> Students { get; set; }
     }
 }
