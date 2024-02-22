6e9b3b4c94a6b0dd33485f8deed170812c824bb4
Dennis-Soare <79460847+Dennis-Soare@users.noreply.github.com>
2024-02-22 12:00:36 +0200

    added student controller

---
+++ DawProjectAPI/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using DawProjectAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DawProjectAPI.Controllers
+{
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public IEnumerable<Student> GetStudents()
+        {  
+            return new List<Student> { 
+                new Student{Id=1, Name="Name 1"} ,
+                new Student{Id=2, Name="Name 2"}
+            }; 
+        }
+
+    }
+}
---
+++ DawProjectAPI/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace DawProjectAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
