1d10863852e3f7557ed112a705ba8018a5f9ea73
Dennis-Soare <79460847+Dennis-Soare@users.noreply.github.com>
2024-02-23 22:52:37 +0200

    added routing

--- DawFrontend/src/app/app-routing.module.ts
+++ DawFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './components/students/students.component';
+import { SubjectsComponent } from './components/subjects/subjects.component';
+import { BooksComponent } from './components/books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'students',
+    component: StudentsComponent
+  },
+  {
+    path: 'subjects',
+    component: SubjectsComponent
+  },
+  {
+    path: 'books',
+    component: BooksComponent
+  },
+  {
+    path: '**',
+    component: StudentsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
