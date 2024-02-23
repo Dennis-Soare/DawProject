22ab6e9705af8c3fc703de273a44d1173b1887f2
Dennis-Soare <79460847+Dennis-Soare@users.noreply.github.com>
2024-02-23 22:45:30 +0200

    added components

--- DawFrontend/src/app/app.module.ts
+++ DawFrontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './components/students/students.component';
+import { SubjectsComponent } from './components/subjects/subjects.component';
+import { BooksComponent } from './components/books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    SubjectsComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
