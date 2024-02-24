9076093fb4c3bd4dd4467c6527020d71a8570006
Dennis-Soare <79460847+Dennis-Soare@users.noreply.github.com>
2024-02-25 00:26:26 +0200

    used include

---
+++ DawProjectAPI/Repositories/IStudentRepository.cs
@@ -0,0 +1,10 @@
+﻿using DawProjectAPI.Models;
+using Demo.Repository.GenericRepository;
+
+namespace DawProjectAPI.Repositories
+{
+    public interface IStudentRepository : IGenericRepository<Student>
+    {
+        public Task<List<Student>> GetStudentsWithSubjecs();
+    }
+}
---
+++ DawProjectAPI/Repositories/StudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using DawProjectAPI.Data;
+using DawProjectAPI.Models;
+using Demo.Repository.GenericRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace DawProjectAPI.Repositories
+{
+    public class StudentRepository : GenericRepository<Student>, IStudentRepository
+    {
+        public StudentRepository(ApplicationDbContext context) : base(context) { }
+        
+        public async Task<List<Student>> GetStudentsWithSubjecs()
+        {
+            return await _table.Include(x => x.StudentSubjects).ThenInclude(x => x.Student).ToListAsync();
+        }
+    }
+}
